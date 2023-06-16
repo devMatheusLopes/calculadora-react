5e4e1d0d6e1c3b15102d1e7c77bfdadc1506fad1
MatheusLopes13 <matheuuslopes13@gmail.com>
2023-06-16 16:30:42 -0300

    calculadora em react

---
+++ README.md
@@ -0,0 +1,5 @@
+Apos baixar o projeto executar o comando npm install.
+
+Depois de baixar todos pacotes usados no projeto execute o comando npm start.
+
+Automaticamente irá abrir a nossa calculadora no seu navegador favorito.
---
+++ src/App.js
@@ -0,0 +1,66 @@
+
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+
+  const [number_1, setNumber_1] = useState(0)
+  const [number_2, setNumber_2] = useState(0)
+  const [result, setResult] = useState(0)
+  const [action, setAction] = useState("somar")
+
+  useEffect(() => {
+    setResult(calculator())
+  }, [number_1, number_2, action])
+
+  const calculator = () => {
+
+    if (action === "somar") {
+      return parseFloat(number_1) + parseFloat(number_2)
+    }
+    else if (action === "subtrair") {
+      return parseFloat(number_1) - parseFloat(number_2)
+    }
+    else if (action === "dividir") {
+      return parseFloat(number_1) / parseFloat(number_2)
+    }
+    else {
+      setResult(0)
+    }
+
+  }
+
+  return (
+    <>
+
+      <h1>Calculadora</h1>
+      
+      <div className='main'>
+
+        <label> Numero 1 </label>
+        <input
+          type="number"
+          value={number_1}
+          onChange={(e) => setNumber_1(e.target.value)} />
+        
+        <label> Numero 2 </label>
+        <input
+          type="number"
+          value={number_2}
+          onChange={(e) => setNumber_2(e.target.value)} />
+
+        <select onChange={(e) => setAction(e.target.value)}>
+          <option>somar</option>
+          <option>subtrair</option>
+          <option>dividir</option>
+        </select>
+
+        <h2> Resultado : {result} </h2>
+
+      </div>
+
+    </>
+  );
+}
+
+export default App;
